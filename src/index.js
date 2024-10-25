2c06afbc8c79c72633f7c822761e2f5b352b0b51
Mohanna77kian <mohana.kian88@gmail.com>
2024-10-25 19:29:46 +0330

    add api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-function searchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = searchInput.value;
-}
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+let result;
+async function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  let temp = document.querySelector(".weather-temperature");
+  let icon = document.querySelector(".weather-icon");
+
+  let apiAddress = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=6bdb310086a38f79b2oc40bdd04tdc66`;
+  result = await fetch(apiAddress);
+
+  result = await result.json();
+  console.log(result);
+
+  cityElement.innerHTML = result.city;
+  temp.innerHTML = result.temperature.current;
+  icon.src = result.condition.icon_url;
+}
